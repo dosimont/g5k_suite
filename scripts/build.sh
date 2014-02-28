932469625e4db29213414fa510811deb0231fff3
Damien Dosimont <damien.dosimont@imag.fr>
2014-02-28 14:05:04 +0100

    pajeng

--- scripts/build.sh
+++ scripts/build.sh
@@ -33,6 +33,18 @@ make &&
 make install &&
 cd ../..
 }
+
+function pajeng
+{
+cd pajeng &&
+mkdir -p build &&
+cd build &&
+cmake .. &&
+make &&
+make install &&
+cd ../..
+}
+
 cd mpi_tools &&
 for i in `ls *tar.gz`
 do
@@ -52,11 +64,13 @@ apt-get install zlib1g-dev &&
 apt-get install libbfd-dev &&
 apt-get install gfortran &&
 apt-get install cmake &&
+apt-get install build-essential libqt4-dev libboost-dev freeglut3-dev &&
 tarins openmpi-1.6.5
 tarins scorep-1.0.2
 aky
 poti
 cp /usr/local/lib/libpoti.so.4.2 /usr/local/lib/libpoti.so.2
+pajeng
 
 
 cd ..
